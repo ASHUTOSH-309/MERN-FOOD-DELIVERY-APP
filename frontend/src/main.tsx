c9f3fbdc5baf98a8c312fa8733e1a6804d074e5b
ASHUTOSH-309 <ashutosh.sharma83020@gmail.com>
2024-07-25 17:20:11 +0530

    logic to Pre-populateform with the existing data in the database

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import AppRoutes from './AppRoutes'
 import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { Toaster } from './components/ui/sonner'
 
 const queryClient = new QueryClient({
 
@@ -31,7 +32,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
               <Auth0ProviderWithNavigate>
 
                 <AppRoutes />
-
+                  <Toaster visibleToasts={1} position='top-right' richColors />
               </Auth0ProviderWithNavigate>
       </QueryClientProvider>
     </Router>

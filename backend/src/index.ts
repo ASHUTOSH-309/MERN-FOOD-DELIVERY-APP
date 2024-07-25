8dd49c8bc472575a805e33ead73b2e3acfeb00c3
ASHUTOSH-309 <ashutosh.sharma83020@gmail.com>
2024-07-25 16:16:39 +0530

    Complete till user creation and updation

---
+++ backend/src/index.ts
@@ -0,0 +1,33 @@
+
+import express from "express"
+import { Request, Response } from "express";
+import cors from "cors"
+import "dotenv/config"
+import myUserRoute from "./routes/MyUserRoute"
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGODB_CONENCTION_STRING as string)
+.then(()=>{
+
+console.log("connected to database")
+
+})
+
+const app=express()
+app.use(express.json())
+app.use(cors())
+
+/* app.get("/test",async(req: Request,res: Response)=>{
+
+        res.json({message:"Hello!"})
+
+}) */
+
+
+app.use("/api/my/user",myUserRoute)
+
+app.listen(8800,()=>{
+
+    console.log(`server started on localhost:8800`)
+
+})
